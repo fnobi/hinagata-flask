993ebe56a6120c1f6090c24b0345eaa208205922
nobii <fujisawa-shin@kayac.com>
2017-09-05 16:52:12 +0900

    設定値の書き方を色々考える

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 app = Flask(__name__)
 
 # config
 # TODO: development環境だけ？
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:""@localhost/hinagata-flask_development'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.environ.get('DATABASE_DB')
 
 # database
 db = SQLAlchemy(app)
